8172dc80ce7f68429e28b000bc498e131c4ef0f3
platipus25 <40722163+platipus25@users.noreply.github.com>
2025-04-17 14:07:49 -0700

    Add equilibrium finder

---
+++ logistic-equilibrium.py
@@ -0,0 +1,43 @@
+import taichi as ti
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ti.init(arch=ti.gpu)
+ti.init(arch=ti.cpu)
+
+simulation_length = 20000
+num_simulations = 50000
+
+population = ti.field(dtype=ti.f32, shape=(num_simulations, simulation_length))
+
+arr_type = ti.types.ndarray(ndim=1)
+
+
+@ti.kernel
+def paint(r_values: arr_type):
+    for j in r_values:
+        r = r_values[j]
+        for i in range(simulation_length):
+            population[j, i + 1] = 4 * r * population[j, i] * (1 - population[j, i])
+
+
+def main():
+    print("Hello from imaginary-bifurcations!")
+
+    for i in range(num_simulations):
+        population[i, 0] = 0.5
+    r = np.linspace(0.2, 1, num_simulations, dtype=np.float32)
+    # r = 1 - np.logspace(0, 0.8, num_simulations, dtype=np.float32)
+    paint(r_values=r)
+    print("Finished calculations")
+
+    fig, ax = plt.subplots()
+
+    pop = population.to_numpy()
+    ax.plot(pop[:, -50:], ".", ms=2)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
