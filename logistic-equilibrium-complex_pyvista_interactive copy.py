a3363f3ab91e6e866772324d4f46712ed322212b
platipus25 <40722163+platipus25@users.noreply.github.com>
2025-05-01 14:20:40 -0700

    Make the alternate memory layout actually work
    it's very slightly faster
    ~800 ms improvement for ```    params = Parameters(
            simulation_length=5000,
            equilibrium_resolution=6,
            num_simulations=800,
        )
    ```

--- logistic-equilibrium-complex_pyvista_interactive copy.py
+++ logistic-equilibrium-complex_pyvista_interactive copy.py
@@ -4,16 +4,23 @@ import pyvista as pv
 from pyvistaqt import BackgroundPlotter
 from dataclasses import dataclass
 
+import time
+
 
 @njit(parallel=True)
 def paint(population, simulation_length):
     for idx in prange(population.shape[0]):
-        r = 4 * (population[idx, 0, 0] + 1j * population[idx, 1, 0])
+        sim_arr = population[idx, 2, :]
+        r = population[idx, 0, 0] + 1j * population[idx, 1, 0]
         for k in range(simulation_length):
-            size = population.shape[2]
-            x_n = population[idx, 2, k % size]
-            value = r * x_n * (1 - x_n)
-            population[idx, 2, (k + 1) % size] = value
+            size = len(sim_arr)
+            x_n = sim_arr[k % size]
+            value = 4 * r * x_n * (1 - x_n)
+            sim_arr[(k + 1) % size] = value
+
+            if not np.isfinite(value):
+                sim_arr[:] = np.nan
+                break
 
     return population
 
@@ -39,7 +46,9 @@ def create_buffer(params: Parameters):
     )
 
     population = np.stack([grid_re, grid_im, grid_pop], axis=2)
-    population = np.reshape(population, shape=(-1, 3, params.equilibrium_resolution))
+    population = np.reshape(
+        population, shape=(-1, 3, params.equilibrium_resolution), copy=True
+    )
 
     print(population.shape)
 
@@ -47,23 +56,20 @@ def create_buffer(params: Parameters):
 
 
 def create_mesh(params: Parameters, population: np.ndarray):
-    coords = np.reshape(population, (-1, 3))
+    # print(population, np.swapaxes(population, 1, 2).shape)
+    coords = np.reshape(np.swapaxes(population, 1, 2), (-1, 3))
     # check that we haven't made a copy
     assert coords.base is not None
 
-    real_coords = coords.view(np.float64)
-    strided_view = real_coords[:, 0:-1:2]
-    print(real_coords.shape, strided_view.shape)
-    abs_coords = np.abs(coords)
-    print(abs_coords.shape)
-
-    phases = np.angle(coords[:, 2])
-    print(np.real(coords))
+    # real_coords = coords.view(np.float64)
+    # strided_view = real_coords[:, 0:-1:2]
+    # print(real_coords.shape, strided_view.shape)
+    print(coords.shape)
 
     print("Making mesh")
-    mesh = pv.PolyData(np.abs(coords))
-    # mesh.point_data["height"] = coords[:, 2]
-    mesh.point_data["angles"] = phases
+    mesh = pv.PolyData(np.real(coords))
+    mesh.point_data["height"] = np.abs(coords[:, 2])
+    mesh.point_data["angles"] = np.angle(coords[:, 2])
 
     return mesh
 
@@ -74,32 +80,47 @@ def main():
     params = Parameters(
         simulation_length=5000,
         equilibrium_resolution=6,
-        num_simulations=20,
+        num_simulations=500,
     )
 
     params._Parameters__r = (
         np.linspace(-0.5, 1, params.num_simulations),
         np.linspace(-0.5, 0.5, params.num_simulations),
     )
+
+    pl = BackgroundPlotter()
+    pl.show_axes()
+    pl.show_bounds(
+        location="outer",
+        xtitle="real",
+        ytitle="imaginary",
+        ztitle="equilibrium value",
+    )
+    pl.show()
     # print(params)
 
     population = create_buffer(params)
 
-    r_re, r_im = params.r
-    print("Yay")
-    paint(population, params.simulation_length)
-    print("Finished calculations")
+    for i in reversed(range(1, 50)):
+        decimated_population = population[::i]
+        decimated_population[..., 2, :] = 0.5
 
-    # population = simulate(params)
-    mesh = create_mesh(params, population)
+        print("Yay")
+        start_time = time.perf_counter()
+        paint(decimated_population, params.simulation_length)
+        end_time = time.perf_counter()
+        print(f"Finished calculations in {(end_time - start_time) * 1000} ms")
 
-    pl = pv.Plotter()
-    pl.show_axes()
+        # population = simulate(params)
+        # start_time = time.perf_counter()
+        mesh = create_mesh(params, decimated_population)
+        # end_time = time.perf_counter()
+        # print(f"Created mesh in {(end_time - start_time) * 1000} ms")
 
-    print("Inserting mesh")
-    pl.add_mesh(mesh, scalars="angles", name="mymesh")
-    # pl.show_bounds()
-    pl.show()
+        print("Inserting mesh")
+        pl.add_mesh(mesh, scalars="angles", name=f"mymesh{i}")
+        pl.render()
+        pl.app.processEvents()
 
     # print("Creating gif")
     # viewup = [0.5, 0.5, 1]
@@ -109,6 +130,7 @@ def main():
     # pl.open_gif("orbit.gif")
     # pl.orbit_on_path(path, write_frames=True, viewup=[0, 0, 1], step=0.05)
     # pl.close()
+    pl.app.exec_()
 
 
 if __name__ == "__main__":
